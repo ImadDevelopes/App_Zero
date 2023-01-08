2f1fa46e7dc972d5a6962f23032e59fd9fded702
ImadDevelopes <imaduddin.develops@gmail.com>
2023-01-08 21:05:06 +0530

    My First Commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Imaduddin</Text>
+      <Text>Hello Imaduddin. This is into git</Text>
       <StatusBar style="auto" />
     </View>
   );
