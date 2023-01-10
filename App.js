49c599c91468090ec033d1c986686a1a73dd4416
ImadDevelopes <imaduddin.develops@gmail.com>
2023-01-10 07:32:42 +0530

    my second project

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
+  return (git
     <View style={styles.container}>
       <Text>My new Message.</Text>
       <StatusBar style="auto" />
